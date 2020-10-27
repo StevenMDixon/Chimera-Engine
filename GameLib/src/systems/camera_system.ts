e854cfd0fbe45f99df54a59c63925468a7f440e3
StevenDixonDev <34195467+StevenDixonDev@users.noreply.github.com>
2020-10-27 09:41:00 -0400

    adding in options for camera movements

--- GameLib/src/systems/camera_system.ts
+++ GameLib/src/systems/camera_system.ts
@@ -3,7 +3,22 @@ import Store from '../core/store';
 import Vector from "../modules/vector";
 import {createCollidable, convertToCollidable, round} from '../modules/utils';
 
+interface iCameraOptions {
+        deadZone: { // replaces camera border
+            x: number,
+            y: number
+        },
+        damping: { // how fast the camera moves
+            x: number,
+            y: number
+        },
+        lookAhead: {
+            time: number,
+            smoothing: number,
+            ignoreY: boolean
+        }
 
+}
 
 class Camera_System extends System_Base{
     center: any
@@ -20,20 +35,37 @@ class Camera_System extends System_Base{
         }
     }
 
+
+    
+
     update({deltaTime, entities}){
-       const {camera, scale, ctx} = Store.getStore('engine').access('camera', 'scale', 'ctx');
+       const {camera, scale, ctx, cameraBorder, debug} = Store.getStore('engine').access('camera', 'scale', 'ctx', 'cameraBorder', 'debug');
        ctx.strokeStyle = 'red'
        let nc = createCollidable(camera.offSets.x, camera.offSets.y, camera.size.x / scale, camera.size.y / scale); 
         
         entities.query(...this.targetComponents).forEach(e => {
             if(e.hasComponent('Size', 'Position')){
+               
+
                 let p = convertToCollidable(e);
-                camera.offSets.set(Vector.subtract(nc.pos.vLinearInt(p['pos'], .1), Vector.divide(camera.size, 2 * scale)))
+
+                let distance = Vector.subtract(p.pos, nc.pos);
+                //console.log(distance)
+                if((Math.abs(distance.x) >= cameraBorder || Math.abs(distance.y) >= cameraBorder) || cameraBorder == 0){
+                    let diffVec = Vector.subtract(nc.pos.vLinearInt(p['pos'], .1), Vector.divide(camera.size, 2 * scale));
+                    camera.offSets.set(diffVec)
+                }
+                
             }
        })
 
-       
-      ctx.strokeRect(200/ scale, 200/ scale, 1, 1)
+       if(debug){
+        ctx.strokeRect(200/ scale, 200/ scale, 1, 1);
+        ctx.beginPath();
+        ctx.arc(200/ scale, 200/ scale, cameraBorder, 0, 2 * Math.PI);
+        ctx.stroke();
+       }
+      
     }
 }
 
