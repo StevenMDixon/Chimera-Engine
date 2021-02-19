2cf3555e30a5e552c104c85c7c8f0bbfd1bd8cf5
StevenDixonDev <34195467+StevenDixonDev@users.noreply.github.com>
2021-02-19 12:47:16 -0500

    created specsheet to keep up with layout

--- Games/pong/src/scenes/opening.js
+++ Games/pong/src/scenes/opening.js
@@ -1,19 +1,19 @@
 import Chimera from 'ChimeraEngine';
 
-console.log(Chimera)
 class Opening extends Chimera.sceneTemplates.PixiScene {
     constructor(EngineItems){
         super('Opening', EngineItems);
         this.ssheets = {};
+        this.player = null;
     }
 
     preload(){
-        console.log('Global', this.GlobalLoader.shared)
+        console.log('Global', this.global)
         //this.loader.add("player", "images/animationphases.png");
     }
 
     setup(){
-        let sheet = new this.PIXI.BaseTexture.from(this.GlobalLoader.shared.resources["player"].url);
+        let sheet = new this.PIXI.BaseTexture.from(this.global.Loader.shared.resources["player"].url);
         this.ssheets.player = {}
         let h = 259;
         let w = 130;
@@ -29,32 +29,41 @@ class Opening extends Chimera.sceneTemplates.PixiScene {
         player.y = 200;
         player.loop = true;
         player.anchor.set(0.5);
-        player.animationSpeed = .1;
+        player.animationSpeed = .001;
         player.tint = 0x1FFFFF;
         this.layers.bg1.addChild(player);
-        player.play();
-
+        //player.play();
+        this.player = player;
         //setTimeout(()=> {player.stop()}, 1000)
+        // player.onComplete = function () {
+        //     console.log('completed')
+        //   };
 
 
-        //let player2 = new this.PIXI.AnimatedSprite(this.ssheets.player.one);
-        // player2.x = 200;
-        // player2.y = 200;
 
-        // this.layers.bg2.addChild(player2);
+        let player2 = new this.PIXI.AnimatedSprite(this.ssheets.player.one);
+        player2.x = 200;
+        player2.y = 200;
 
-        // let player3 = new this.PIXI.AnimatedSprite(this.ssheets.player.one);
-        // player3.x = 100;
-        // player3.y = 100;
+        this.layers.bg2.addChild(player2);
 
-        // this.layers.effect.addChild(player3);
+        let player3 = new this.PIXI.AnimatedSprite(this.ssheets.player.one);
+        player3.x = 100;
+        player3.y = 100;
+
+        this.layers.effect.addChild(player3);
 
 
-        // let player4 = new this.PIXI.AnimatedSprite(this.ssheets.player.one);
-        // player4.x = 0;
-        // player4.y = 0;
+        let player4 = new this.PIXI.AnimatedSprite(this.ssheets.player.one);
+        player4.x = 0;
+        player4.y = 0;
 
-        // this.createLayer('Test', 1, player4);
+        this.createLayer('Test', 1, player4);
+    }
+
+    update(dt){
+        //this.layers.bg1.rotation += .01;
+        this.player.update(dt);
     }
 }
 
